0143d73c9d02a58f24759fa6ccb88fea654aa20c
OwemShu <shuhenxin@qq.com>
2022-11-18 16:49:57 +0800

    完成 home 页面开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -16,6 +16,27 @@
         <image :src="item.image_src" class="nav-img"></image>
       </view>
     </view>
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <!-- 楼层 item 项 -->
+      <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+        <!-- 楼层标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片的盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧 4 个小图片的盒子 -->
+          <view class="right-img-box">
+            <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+              <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -27,12 +48,15 @@
         swiperList: [],
         // 分类导航的数据列表
         navList: [],
+        // 楼层的数据列表
+        floorList: [],
       }
     },
     onLoad() {
       // 在小程序页面刚加载的时候，调用获取轮播图数据的方法
       this.getSwiperList(),
-        this.getNavList()
+        this.getNavList(),
+        this.getFloorList()
     },
     methods: {
       // 获取轮播图数据的方法
@@ -49,6 +73,20 @@
         } = await uni.$http.get('/api/public/v1/home/catitems')
         if (res.meta.status !== 200) return uni.$showMsg()
         this.navList = res.message
+      },
+      // 获取楼层列表数据
+      async getFloorList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/floordata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 通过双层 forEach 循环，处理 URL 地址
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
       },
       navClickHandler(item) {
         if (item.name === '分类') {
@@ -81,5 +119,22 @@
       width: 128rpx;
       height: 140rpx;
     }
+  }
+
+  .floor-title {
+    height: 60rpx;
+    width: 100%;
+    display: flex;
+  }
+
+  .right-img-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
   }
 </style>
