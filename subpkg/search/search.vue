b09513a846f9568a841c757fa1670690797fffe9
OwemShu <shuhenxin@qq.com>
2022-11-19 18:44:38 +0800

    解决 search 搜索关键字的展示, 存入, 顺序及防止重复的问题

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -3,10 +3,22 @@
     <!-- 使用 uni-ui 提供的搜索组件 -->
     <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
     <!-- 搜索建议列表 -->
-    <view class="sugg-list">
+    <view class="sugg-list" v-if="searchResults.length !== 0">
       <view class="sugg-item" v-for="(item, i) in searchResults" :key="i" @click="gotoDetail(item.goods_id)">
         <view class="goods-name">{{item.goods_name}}</view>
         <uni-icons type="arrowright" size="16"></uni-icons>
+      </view>
+    </view>
+    <!-- 搜索历史 -->
+    <view class="history-box" v-else>
+      <!-- 标题区域 -->
+      <view class="history-title">
+        <text>搜索历史</text>
+        <uni-icons type="trash" size="17"></uni-icons>
+      </view>
+      <!-- 列表区域 -->
+      <view class="history-list">
+        <uni-tag :text="item" v-for="(item, i) in historys" :key="i"></uni-tag>
       </view>
     </view>
   </view>
@@ -21,8 +33,17 @@
         // 搜索关键词
         kw: '',
         // 搜索结果列表
-        searchResults: []
+        searchResults: [],
+        // 搜索关键词的历史记录
+        historyList: ['a', 'app', 'apple']
       };
+    },
+    computed: {
+      historys() {
+        // 注意：由于数组是引用类型，所以不要直接基于原数组调用 reverse 方法，以免修改原数组中元素的顺序
+        // 而是应该新建一个内存无关的数组，再进行 reverse 反转
+        return [...this.historyList].reverse()
+      }
     },
     methods: {
       input(e) {
@@ -51,6 +72,22 @@
         })
         if (res.meta.status !== 200) return uni.$showMsg()
         this.searchResults = res.message
+        // 查询到搜索建议之后，调用 saveSearchHistory() 方法保存搜索关键词
+        this.saveSearchHistory()
+      },
+      // 保存搜索关键词的方法
+      // 保存搜索关键词为历史记录
+      saveSearchHistory() {
+        // this.historyList.push(this.kw)
+
+        // 1. 将 Array 数组转化为 Set 对象
+        const set = new Set(this.historyList)
+        // 2. 调用 Set 对象的 delete 方法，移除对应的元素
+        set.delete(this.kw)
+        // 3. 调用 Set 对象的 add 方法，向 Set 中添加元素
+        set.add(this.kw)
+        // 4. 将 Set 对象转化为 Array 数组
+        this.historyList = Array.from(set)
       },
       gotoDetail(goods_id) {
         uni.navigateTo({
@@ -88,6 +125,29 @@
         // 文本溢出后，使用 ... 代替
         text-overflow: ellipsis;
         margin-right: 3px;
+      }
+    }
+  }
+
+  .history-box {
+    padding: 0 5px;
+
+    .history-title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 40px;
+      font-size: 13px;
+      border-bottom: 1px solid #efefef;
+    }
+
+    .history-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .uni-tag {
+        margin-top: 5px;
+        margin-right: 5px;
       }
     }
   }
