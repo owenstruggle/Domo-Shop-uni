9411237321c8b31b9661ce97b17d03257c5fc750
OwemShu <shuhenxin@qq.com>
2022-11-19 18:16:18 +0800

    渲染搜索页面的基本结构

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,7 @@
 <template>
-  <view>
-    
+  <view class="search-box">
+    <!-- 使用 uni-ui 提供的搜索组件 -->
+    <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
   </view>
 </template>
 
@@ -8,12 +9,22 @@
   export default {
     data() {
       return {
-        
+
       };
+    },
+    methods: {
+      input(e) {
+        // e.value 是最新的搜索内容
+        console.log(e.value)
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+  .search-box {
+    position: sticky;
+    top: 0;
+    z-index: 999;
+  }
 </style>
--- uni_modules/uni-search-bar/components/uni-search-bar/uni-search-bar.vue
+++ uni_modules/uni-search-bar/components/uni-search-bar/uni-search-bar.vue
@@ -109,8 +109,8 @@
 		},
 		data() {
 			return {
-				show: false,
-				showSync: false,
+				show: true,
+				showSync: true,
 				searchVal: ''
 			}
 		},
@@ -236,6 +236,8 @@
 		position: relative;
 		padding: 10px;
 		// background-color: #fff;
+    /* 将默认的 #FFFFFF 改为 #C00000 */
+      background-color: #c00000;
 	}
 
 	.uni-searchbar__box {
