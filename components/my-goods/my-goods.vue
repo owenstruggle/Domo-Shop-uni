fff42a38136d82e67408b1b898177febe86d15d8
OwemShu <shuhenxin@qq.com>
2022-11-19 19:18:21 +0800

    完成了 goods 页面中的上拉加载数据的功能

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -10,7 +10,7 @@
       <view class="goods-name">{{goods.goods_name}}</view>
       <view class="goods-info-box">
         <!-- 商品价格 -->
-        <view class="goods-price">￥{{goods.goods_price}}</view>
+        <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
       </view>
     </view>
   </view>
@@ -30,6 +30,12 @@
       return {
         // 默认的空图片
         defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+      }
+    },
+    filters: {
+      // 把数字处理为带两位小数点的数字
+      tofixed(num) {
+        return Number(num).toFixed(2)
       }
     },
   }
