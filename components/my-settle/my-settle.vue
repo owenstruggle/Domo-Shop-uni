7fa6a6faed6defa5be3b548a996b3ca51350538e
OwemShu <shuhenxin@qq.com>
2022-11-21 00:18:06 +0800

    微信支付

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -50,6 +50,61 @@
         // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
         this.updateAllGoodsState(!this.isFullCheck)
       },
+      // 微信支付
+      async payOrder() {
+        // 1. 创建订单
+        // 1.1 组织订单的信息对象
+        const orderInfo = {
+          // 开发期间，注释掉真实的订单价格，
+          // order_price: this.checkedGoodsAmount,
+          // 写死订单总价为 1 分钱
+          order_price: 0.01,
+          consignee_addr: this.addstr,
+          goods: this.cart.filter(x => x.goods_state).map(x => ({
+            goods_id: x.goods_id,
+            goods_number: x.goods_count,
+            goods_price: x.goods_price
+          }))
+        }
+        // 1.2 发起请求创建订单
+        const {
+          data: res
+        } = await uni.$http.post('/api/public/v1/my/orders/create', orderInfo)
+        if (res.meta.status !== 200) return uni.$showMsg('创建订单失败！')
+        // 1.3 得到服务器响应的“订单编号”
+        const orderNumber = res.message.order_number
+
+        // 2. 订单预支付
+        // 2.1 发起请求获取订单的支付信息
+        const {
+          data: res2
+        } = await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', {
+          order_number: orderNumber
+        })
+        // 2.2 预付订单生成失败
+        if (res2.meta.status !== 200) return uni.$showError('预付订单生成失败！')
+        // 2.3 得到订单支付相关的必要参数
+        const payInfo = res2.message.pay
+
+        // 3. 发起微信支付
+        // 3.1 调用 uni.requestPayment() 发起微信支付
+        const [err, succ] = await uni.requestPayment(payInfo)
+        // 3.2 未完成支付
+        if (err) return uni.$showMsg('订单未支付！')
+        // 3.3 完成了支付，进一步查询支付的结果
+        const {
+          data: res3
+        } = await uni.$http.post('/api/public/v1/my/orders/chkOrder', {
+          order_number: orderNumber
+        })
+        // 3.4 检测到订单未支付
+        if (res3.meta.status !== 200) return uni.$showMsg('订单未支付！')
+        // 3.5 检测到订单支付完成
+        uni.showToast({
+          title: '支付完成！',
+          icon: 'success'
+        })
+      },
       // 点击了结算按钮
       settlement() {
         // 1. 先判断是否勾选了要结算的商品
@@ -58,9 +113,12 @@
         // 2. 再判断用户是否选择了收货地址
         if (!this.addstr) return uni.$showMsg('请选择收货地址！')
 
-        // 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+        // 3. 最后判断用户是否登录了
         // if (!this.token) return uni.$showMsg('请先登录！')
         if (!this.token) return this.delayNavigate()
+
+        // 4. 实现微信支付功能
+        this.payOrder()
       },
       // 展示倒计时的提示消息
       showTips(n) {
