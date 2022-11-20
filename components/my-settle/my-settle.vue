8964b88b3d14f46a16e76c1eaccc709dbc54f61d
OwemShu <shuhenxin@qq.com>
2022-11-20 23:40:49 +0800

    渲染结算区域的结构和样式

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -1,28 +1,62 @@
 <template>
   <!-- 最外层的容器 -->
   <view class="my-settle-container">
-    结算组件
+    <!-- 全选区域 -->
+    <label class="radio">
+      <radio color="#C00000" :checked="true" /><text>全选</text>
+    </label>
+
+    <!-- 合计区域 -->
+    <view class="amount-box">
+      合计:<text class="amount">￥1234.00</text>
+    </view>
+
+    <!-- 结算按钮 -->
+    <view class="btn-settle">结算(0)</view>
   </view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {}
-  },
-}
+  export default {
+    data() {
+      return {}
+    },
+  }
 </script>
 
 <style lang="scss">
-.my-settle-container {
-  /* 底部固定定位 */
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  /* 设置宽高和背景色 */
-  width: 100%;
-  height: 50px;
-  background-color: cyan;
-}
+  .my-settle-container {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    // 将背景色从 cyan 改为 white
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 5px;
+    font-size: 14px;
+
+    .radio {
+      display: flex;
+      align-items: center;
+    }
+
+    .amount {
+      color: #c00000;
+    }
+
+    .btn-settle {
+      height: 50px;
+      min-width: 100px;
+      background-color: #c00000;
+      color: white;
+      line-height: 50px;
+      text-align: center;
+      padding: 0 10px;
+    }
+  }
 </style>
 </style>
