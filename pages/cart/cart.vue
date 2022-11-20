c30a934213498b61c63c37dbf44ec2e6cb808e01
OwemShu <shuhenxin@qq.com>
2022-11-20 23:47:33 +0800

    渲染购物车为空时的页面结构

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,8 +1,8 @@
 <template>
-  <view class="cart-container">
+  <view class="cart-container" v-if="cart.length !== 0">
     <!-- 收货地址组件 -->
     <my-address></my-address>
-    
+
     <!-- 商品列表的标题区域 -->
     <view class="cart-title">
       <!-- 左侧的图标 -->
@@ -21,6 +21,12 @@
     </uni-swipe-action>
     <!-- 结算区域 -->
     <my-settle></my-settle>
+  </view>
+
+  <!-- 空白购物车区域 -->
+  <view class="empty-cart" v-else>
+    <image src="/static/cart_empty@2x.png" class="empty-img"></image>
+    <text class="tip-text">空空如也~</text>
   </view>
 </template>
 
@@ -93,5 +99,23 @@
 
   .cart-container {
     padding-bottom: 50px;
+  }
+
+  .empty-cart {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 150px;
+
+    .empty-img {
+      width: 90px;
+      height: 90px;
+    }
+
+    .tip-text {
+      font-size: 12px;
+      color: gray;
+      margin-top: 15px;
+    }
   }
 </style>
