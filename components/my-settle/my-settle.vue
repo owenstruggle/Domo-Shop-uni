2879938c0b6d7f1e91a55720a0908f4c15bbbdea
OwemShu <shuhenxin@qq.com>
2022-11-20 23:39:23 +0800

    完成收货地址区域设计

---
+++ components/my-settle/my-settle.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 最外层的容器 -->
+  <view class="my-settle-container">
+    结算组件
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss">
+.my-settle-container {
+  /* 底部固定定位 */
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  /* 设置宽高和背景色 */
+  width: 100%;
+  height: 50px;
+  background-color: cyan;
+}
+</style>
+</style>
